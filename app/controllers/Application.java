7c8e0b9d1a8fad516d08528febb882419de226ab
Jörgen Lundberg <jorgen.lundberg@gmail.com>
2012-03-18 19:19:01 +0100

    Player name form.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -10,5 +10,9 @@ public class Application extends Controller {
   public static Result index() {
     return ok(index.render("Your new application is ready."));
   }
+
+  public static Result storeName() {
+      return ok();
+  }
   
 }
